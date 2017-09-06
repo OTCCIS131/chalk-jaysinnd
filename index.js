a30bac388da33e0b50df72c9d656f30466befbef
Unknown <jdeansart@gmail.com>
2017-09-06 13:18:00 -0500

    happy birthday song for Adam

---
+++ index.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.red('Happy', chalk.blue('Birthday', chalk.yellow('to you')) + '!'));
+console.log(chalk.cyan('Happy', chalk.green('Birthday', chalk.yellow('to you')) + '!'));
+console.log(chalk.red('Happy', chalk.blue('Birthday', chalk.yellow('dear, Adam')) + ','));
+console.log(chalk.red('Happy', chalk.blue('Birthday', chalk.magenta('TO YOU')) + '!'));
+console.log(chalk.yellow(' '));
+console.log(chalk.blue('My story - I, along with others, ended up at OTC because the job that I worked at for the past 12 years'))
+console.log(chalk.yellow('decided they would sell out to a company that had several shops in Mexico.'))
+console.log(chalk.magenta('Once this happened, the state stepped in and said we all qualified for the TRA program (trade readjustment act).'))
+console.log(chalk.green('This would allow myself and others from Polar Tank to go to college (even if it is a bit later in life for some of us).'))
